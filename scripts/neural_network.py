d360da61b6c451cfa6021ebc380fea64d63dfe46
Pankaj <gabalepankaj@gmail.com>
2020-11-16 21:59:50 +1100

    removed tensorflow dependency

--- scripts/neural_network.py
+++ scripts/neural_network.py
@@ -5,8 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 import time
-import tensorflow.compat.v1 as tf
-tf.disable_v2_behavior()
+import tensorflow as tf
 
 # pylint: skip-file
 
